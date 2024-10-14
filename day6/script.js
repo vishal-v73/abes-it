c6fde29e6c242617e2997cadb06c8c3fb6e8443d
vishal7348 <visu835483@gmail.com>
2024-10-14 13:07:22 +0530

    coomit6

---
+++ day6/script.js
@@ -0,0 +1,58 @@
+// // let a = () =>{
+// //     console.log("arrow function")
+// // };
+
+// // let b =()=> console.log("arrow function 2")
+// // b();
+// // a();
+
+
+
+// // letc=(x,y)=>
+// // {
+// //     return x+y;
+// // }
+// // let res=c('rahul','ravi');
+// // console.log(res)
+// // let a=()=>{
+// //     console.log("arrow function")
+// // };
+// // let b=()=>console.log("arr func 2");
+// // a();
+// // b();
+
+// // <<<<<<<<------------------------------------------------------------------------------------------------------------------->>>>>>>>>
+
+// // let c=(x,y)=>{
+// //     return x+y;
+// // }
+// // let res=c('vishal','saumya');
+// // let res1=c(10,50);
+// // console.log(res,res1)
+
+// // // <<<<<<<<------------------------------------------------------------------------------------------------------------------->>>>>>>>>
+
+// // function display(){
+// //     let p1=document.querySelectorAll('p');
+// //     p1[0].innerHTML='i am a paragraph';
+// //     p1[0].style.backgroundColor='orange';
+// //     p1[1].innerHTML='i am a second paragraph';
+// //     p1[1].style.backgroundColor='green';
+// // }
+// // function changeheader(){
+// //     let head1=document.querySelector('h1');
+// //     head1.textContent="changed title";
+// //     let a1=document.querySelector('a');
+// //     a1.setAttribute('href','http://aktu.ac.in');
+// //     a1.innerHTML='aktu website';
+// // };
+
+
+
+// let addcontent = () =>{
+//     const newDiv =  document.createElement('div');
+//     newDiv.textContent = 'this is a new content';
+//     const body = document.querySelector('body');
+//     body.appendChild(newDiv);
+// }
+
